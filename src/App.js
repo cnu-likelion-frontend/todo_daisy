cb15d834001cdc85229705191d524b19eaeebf13
soyeeee2 <soyeeee2@gmail.com>
2024-05-02 21:42:04 +0900

    list added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,49 @@
 import './App.css';
-import Responsive from './MediaQuery'; 
+import { useState, useRef } from 'react';
+import Responsive from './MediaQuery';
+
 
 const { Desktop, Mobile } = Responsive;
 
+const dummys = [
+  'ride a bike',
+  'take a rest',
+  'eat the world',
+  'kakao',
+  'daum',
+  'likelion'
+]
 
 function App() {
+  const [lists, setLists] = useState(dummys);
+  const inputRef = useRef();
 
+  //filtered list required
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newTodo = inputRef.current.value;
+
+    //setLists update
+    setLists([...lists, newTodo])
+    
+    inputRef.current.value = "";
+  }
 
   return (
     <>
       <Desktop>
-        <p>HI Desktop</p>
+        <form action="" onSubmit={handleSubmit}>
+          <input ref={inputRef} type="text" placeholder='Create a new todo...' />
+        </form>
+        <ul>
+          {lists.map((list, index) => (
+            <li key={index} className='flex'>
+              <input type="checkbox" name="" id="" />
+              <p>{list}</p>
+            </li>
+          ))}
+        </ul>
       </Desktop>
       <Mobile>
         HI Mobile
