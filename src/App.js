ef53fa8db1575a9b98a77a7e49dfb9d2daac58d6
SEOHYON OH <skibb27@gamil.com>
2024-05-02 22:37:37 +0900

    input style

--- src/App.js
+++ src/App.js
@@ -10,10 +10,30 @@ function App() {
   return (
     <>
       <Desktop>
-        <p>HI Desktop</p>
+      <div className='to-do-wrapper'>
+        <div className='background-img'></div>
+        <div className='main'>
+          <div className='title'>TODO</div>
+            <div className="container">
+              <div className='todo-input-wrapper'>
+                <input class="todo-check" type="checkbox" value=""></input>
+                <label for="check" class="checkbox-label"></label>
+                <input  type='text' className='todo-input' placeholder='Create a new todo…'></input>
+              </div>
+              <ul className="todo-list">
+                <li className="todo-item">할일1</li>
+                <li className="todo-item">할일2</li>
+              </ul>
+              <footer>
+               <p>Copyright © 2024</p>
+            </footer>
+            </div>
+          </div>
+
+      </div>
       </Desktop>
       <Mobile>
-        HI Mobile
+    
       </Mobile>
     </>
   );
