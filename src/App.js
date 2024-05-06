45e11a01af2e9ab3dae3bb6b3bf4c09b17cc3638
SEOHYON OH <skibb27@gamil.com>
2024-05-06 23:13:53 +0900

    mobile

--- src/App.js
+++ src/App.js
@@ -39,8 +39,10 @@ function App() {
             <div className="container">
               <div className='todo-input-wrapper'>
                 <div className='todo-input-item'>
+                  <div className='todo-input-check'>
                   <input className="todo-check" type="checkbox" id="check" value=""></input>
                   <label htmlFor="check" className="checkbox-label"></label>
+                  </div>
                   <input ref={inputRef} type='text' className='todo-input' placeholder='Create a new todo…'></input>
                 </div>
               </div>
@@ -72,7 +74,43 @@ function App() {
       </div>
       </Desktop>
       <Mobile>
-    
+        <div className='mobile-background-img'></div>
+        <div className='mobile-main'>
+          <div className='mobile-title'>TODO</div>
+            <div className="mobile-container">
+              <div className='mobile-todo-input-wrapper'>
+                <div className='todo-input-item'>
+                <div className='todo-input-check'>
+                  <input className="todo-check" type="checkbox" id="check" value=""></input>
+                  <label htmlFor="check" className="checkbox-label"></label>
+                  </div>
+                  <input ref={inputRef} type='text' className='todo-input' placeholder='Create a new todo…'></input>
+                </div>
+              </div>
+              <ul className="todo-list">
+                {lists.map((item, index) => (
+                  <li key={index} className="todo-item">
+                    <input className="todo-check" type="checkbox" id={`check${index + 1}`} value=""></input>
+                    <label htmlFor={`check${index + 1}`} className="checkbox-label"></label>
+                    {item}
+                  </li>
+                ))}
+                <li className='footer'>
+                  <div className='count'>
+                      <p className='count-item'>5</p><p>items left</p>
+                  </div>
+                  <div className='buttons'>
+                    <button>All</button>
+                    <button>Active</button>
+                    <button>Completed</button>
+                  </div>
+                  <div className='clear'>
+                  <button>Clear Completed</button>
+                  </div>
+                </li>
+              </ul> 
+            </div>
+          </div>
       </Mobile>
     </>
   );
