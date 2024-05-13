c5ea31040a28edf77ec78bfeb9ccbef4c4d164a0
soyeeee2 <soyeeee2@gmail.com>
2024-05-13 14:00:17 +0900

    fix: tag error

--- src/App.js
+++ src/App.js
@@ -167,55 +167,59 @@ function App() {
                     value=""
                   ></input>
                   <label htmlFor="check" className="checkbox-label"></label>
-                  </div>
-                  <input ref={inputRef} type='text' className='mobile-todo-input' placeholder='Create a new todo…'></input>
                 </div>
                 <input
                   ref={inputRef}
                   type="text"
-                  className="todo-input"
+                  className="mobile-todo-input"
                   placeholder="Create a new todo…"
                 ></input>
               </div>
-            </div>
-            <ul className="todo-list">
-              {lists.map((item, index) => (
-                <li key={item.id} className="todo-item">
-                  <input
-                    className="todo-check"
-                    type="checkbox"
-                    id={`check${index + 1}`}
-                    value=""
-                  ></input>
-                  <label
-                    htmlFor={`check${index + 1}`}
-                    className="checkbox-label"
-                  ></label>
-                  {item.text}
-                </li>
-              ))}
-              <li className="footer">
-                <div className="count">
-                  <p className="count-item">{lists.length}</p>
-                  <p>items left</p>
-                </div>
-                <div className="buttons">
-                  <button>All</button>
-                  <button>Active</button>
-                  <button>Completed</button>
-                </div>
-                <div className="clear">
-                  <button>Clear Completed</button>
-                  </div>
-                </li>
-              </ul> 
-              <div className='mobile-buttons'>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-              </div>
+              <input
+                ref={inputRef}
+                type="text"
+                className="todo-input"
+                placeholder="Create a new todo…"
+              ></input>
             </div>
           </div>
+          <ul className="todo-list">
+            {lists.map((item, index) => (
+              <li key={item.id} className="todo-item">
+                <input
+                  className="todo-check"
+                  type="checkbox"
+                  id={`check${index + 1}`}
+                  value=""
+                ></input>
+                <label
+                  htmlFor={`check${index + 1}`}
+                  className="checkbox-label"
+                ></label>
+                {item.text}
+              </li>
+            ))}
+            <li className="footer">
+              <div className="count">
+                <p className="count-item">{lists.length}</p>
+                <p>items left</p>
+              </div>
+              <div className="buttons">
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+              </div>
+              <div className="clear">
+                <button>Clear Completed</button>
+              </div>
+            </li>
+          </ul>
+          <div className="mobile-buttons">
+            <button>All</button>
+            <button>Active</button>
+            <button>Completed</button>
+          </div>
+        </div>
       </Mobile>
     </>
   );
